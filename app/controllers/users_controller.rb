a4ad79116895374ed1f5df9d8642329de5d3ad89
github username <github email address>
2018-02-28 12:59:16 -0700

    signup page complete, able to instantiate new users from it and redirect to login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect '/notes'
+    else
+      erb :"/users/signup"
+    end
+  end
 
+  post '/signup' do
+    if !User.find_by(:username => params[:username])
+      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+    end
+      redirect '/login'
+  end
+
+
+
+
+
+  helpers do
+     def logged_in?
+       !!current_user
+     end
+
+     def current_user
+       @current_user ||= User.find(session[:user_id]) if session[:user_id]
+     end
+
+     def logout!
+       session.clear
+     end
+   end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,12 @@
 class Note < ActiveRecord::Base
 
   belongs_to :user
+
+  def slug
+    self.title.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{ |instance| instance.slug == slug}
+  end
 end
