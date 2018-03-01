451acc49de3aa02cbbb1cfe65b76490cc933b7f0
github username <github email address>
2018-02-28 21:36:28 -0700

    Done.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -29,6 +29,16 @@ class NotesController < ApplicationController
     end
    end
 
+   get '/notes/:slug/edit' do
+     if logged_in?
+       @user = current_user
+       @note = @user.notes.find_by_slug(params[:slug])
+       erb :'/notes/edit'
+     else
+       redirect '/login'
+     end
+   end
+
   get '/notes/:slug' do
     if logged_in?
       @user = current_user
@@ -39,5 +49,30 @@ class NotesController < ApplicationController
     end
   end
 
+  patch '/notes/:slug' do
+    @note = Note.find_by_slug(params[:slug])
+    @user = current_user
+    if !params[:content].empty? && !params[:title].empty?
+      if @note.user_id == @user.id
+        @note.update(:title => params[:title], :content => params[:content])
+        @user.save
+      end
+    end
+      redirect '/notes'
+  end
+
+  delete '/notes/:slug/delete' do
+    @user = current_user
+    if logged_in?
+      if @user.notes.find_by_slug(params[:slug])
+        @note = @user.notes.find_by_slug(params[:slug])
+      end
+      if @note.user_id == @user.id
+        @note.delete
+        @user.save
+      end
+    end
+    redirect '/notes'
+  end
 
 end
